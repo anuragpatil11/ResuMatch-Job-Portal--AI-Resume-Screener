a9d24ab8466b2eba53baf4895af3deda50fdc942
Anurag <anuragpatil.11831183@gmail.com>
2025-09-03 14:22:06 +0530

    chore: clean up code structure and improve readability

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://resummatch.netlify.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
